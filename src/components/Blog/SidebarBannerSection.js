73fe422ac4c10b56de4f3ffa63370f1c691b854f
itssabhishek <itsabhishek0710@gmail.com>
2022-12-21 16:30:52 +0530

    Added Images to all pages.

--- src/components/Blog/SidebarBannerSection.js
+++ src/components/Blog/SidebarBannerSection.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SidebarBanner extends Component {
-
-    render() {
-
-        return (
-            <div className="sidebar__widget mb-55">
-                <div className="sidebar__banner w-img">
-                    <img src="assets/img/blog/banner/banner-1.jpg" alt="img not found"/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="sidebar__widget mb-55">
+        <div className="sidebar__banner w-img">
+          <img src="https://picsum.photos/300/252" alt="img not found" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SidebarBanner;
